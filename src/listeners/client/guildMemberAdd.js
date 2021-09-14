aa6b9f213c997c4a698fdc9948dd683bba374d8e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-14 05:18:34 +0200

    oui

---
+++ src/listeners/client/guildMemberAdd.js
@@ -0,0 +1,16 @@
+const { Listener } = require('discord-akairo');
+
+class GuildMemberAddListener extends Listener {
+    constructor() {
+        super('guildMemberAdd', {
+            emitter: 'client',
+            event: 'guildMemberAdd'
+        });
+    }
+
+    exec(member) {
+        console.log(`${member.username}  a rejoint le serveur`);
+    }
+}
+
+module.exports = GuildMemberAddListener;
---
+++ src/structures/BotClient.js
@@ -0,0 +1,50 @@
+const { AkairoClient, CommandHandler, ListenerHandler, InhibitorHandler} = require('discord-akairo');
+const { Presence } = require('discord.js');
+
+module.exports = class BotClient extends AkairoClient {
+    constructor(config = {}) {
+        super(
+            {   ownerID: '206905331366756353'   }, 
+            {
+                allowedMentions: {
+                    parse:[ 'roles', 'everyone', 'users'],
+                    repliedUser: false
+                },
+                partials : ['CHANNEL','GUILD_MEMBER','MESSAGE','REACTION','USER'],
+                presence: {
+                    status: 'dnd',
+                    activities: [
+                        {
+                            name: "se code gentillement ...",
+                            type:"CUSTOM"
+                            
+                        }
+                    ]
+                },
+                intents: 32767,
+            }
+        );
+
+
+        this.commandHandler = new CommandHandler(this, {
+            allowMention: true,
+            prefix: config.prefix,
+            defaultCooldown: 2000,
+            directory: './src/commands/'
+        });
+
+
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './src/listeners/'
+        });
+
+
+
+
+        this.commandHandler.loadAll();
+
+        this.commandHandler.useListenerHandler(this.listenerHandler);
+        this.listenerHandler.loadAll();
+
+    }
+}
