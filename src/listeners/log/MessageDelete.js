478bcae99141c2476efc334106ae4d0d8ce61d20
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 01:38:06 +0200

    Adding lot of events listeners

---
+++ src/listeners/log/MessageDelete.js
@@ -0,0 +1,18 @@
+const { Listener } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js')
+
+class MessageDeleteListener extends Listener {
+    constructor() {
+        super('messageDelete', {
+            emitter: 'client',
+            event: 'messageDelete'
+        });
+    }
+
+    exec(message) {
+        
+    
+    }
+}
+
+module.exports = MessageDeleteListener; 
---
+++ src/listeners/log/MessageUpdate.js
@@ -0,0 +1,18 @@
+const { Listener } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js')
+
+class MessageUpdateListener extends Listener {
+    constructor() {
+        super('messageUpdate', {
+            emitter: 'client',
+            event: 'messageUpdate'
+        });
+    }
+
+    exec(oldMessage, newMessage) {
+        
+    
+    }
+}
+
+module.exports = MessageUpdateListener;
