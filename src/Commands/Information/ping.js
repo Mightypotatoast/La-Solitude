16a83ff4784ed7cbec407b69d18ece76bab1aca7
MightyPotatoast <chauvet.contact@gmail.com>
2021-10-21 13:18:02 +0200

    ping.js is now working properly

--- src/Commands/Information/ping.js
+++ src/Commands/Information/ping.js
@@ -7,26 +7,13 @@ module.exports = {
     permission: "ADMINISTRATOR",
     active:true,
 
-    async execute(message, client) {
-
-        const messagePing = await message.reply({embeds: [{
-
-            color : 0xFFFFFF,
-            description : "Calculating ping ..."
-
-       }]})
-
-       const ping = Date.now() - message.createdTimestamp
-
-        await message.editReply({
-
-            embeds: [{
-                description: `Bot latency : ${ping} ms , API Latency : ${client.ws.ping}  ms`
-            }]
-
-        })
-       
-
-
+    execute(message, client) {
+        try {
+            message.reply({embeds: [{color : 0xFFFFFF,description : "Calculating ping ..."}], ephemeral: true, fetchReply : true})
+                .then(m => {message.editReply({embeds: [{description: `Bot latency : ${m.createdTimestamp - message.createdTimestamp} ms , API Latency : ${client.ws.ping}  ms`}]})})
+        }catch (e){
+            console.log(e)
+            message.reply({ embeds: [errorEmbed().setDescription(`${e}`)], ephemeral: true })
+        }
     }
 }
