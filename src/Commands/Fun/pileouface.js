94b3c114ba4d5882b28babd720f4327d7227f72c
MightyPotatoast <chauvet.contact@gmail.com>
2021-11-05 15:59:28 +0100

    /coinflip added

---
+++ src/Commands/Fun/pileouface.js
@@ -0,0 +1,28 @@
+const { joinVoiceChannel } = require('@discordjs/voice');
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+
+    name: "coinflip",
+    description: "Flip a coin",
+    permission: "ADMINISTRATOR",
+    active : true,
+
+    async execute(message) {
+        
+
+        try{ 
+            let coin = Math.floor(Math.random() * 2)
+            
+            const coinEmbed = new MessageEmbed()
+                .setColor('#E1A741')
+                .setAuthor("Flip a coin", "https://www.pngall.com/wp-content/uploads/4/Dollar-Gold-Coin-PNG.png")
+                .setDescription(`${message.user} flipped a coin and got a ${coin === 0 ? "Pile" : "Face"}`)
+                .setTimestamp()
+            
+                message.reply({embeds: [coinEmbed]})
+        } catch (error) {
+            console.log(error)
+            message.reply({ embeds: [errorEmbed().setDescription(`${error}`)], ephemeral: true })
+        }
+    }
+}
