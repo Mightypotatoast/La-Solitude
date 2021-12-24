95e540541864d289158be3d322b72294a633ebd7
MightyPotatoast <chauvet.contact@gmail.com>
2021-12-24 17:04:16 +0100

    forgor to remove console.log()

--- src/Events/distube/distube.playSong.js
+++ src/Events/distube/distube.playSong.js
@@ -11,7 +11,6 @@ module.exports = {
  * @param {DisTube.Song} song
  */
     execute(queue,song, client) {
-        console.log(queue);
         if (!queue) return console.log("Queue is not defined")
         //queue.textChannel.send(`Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`)
         console.log(`Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`)
