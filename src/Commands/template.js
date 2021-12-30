f91f719bcec4df04e9b7348919ff4d771075c59b
Ayrox <eweng@live.fr>
2021-12-30 14:31:01 +0100

    add a template for command

---
+++ src/Commands/template.js
@@ -0,0 +1,23 @@
+const { CommandInteraction, MessageEmbed, Client } = require("discord.js");
+const { errorEmbed } = require("../../util/Embeds");
+
+module.exports = {
+    name: "",
+    description: "",
+    permission: "ADMINISTRATOR",
+    active: true,
+
+    options: [],
+
+    /**
+     * 
+     * 
+     * @param {CommandInteraction} message
+     * @param {Client} client
+     * 
+     */
+    async execute(message, client) {
+    
+    
+    }
+}
