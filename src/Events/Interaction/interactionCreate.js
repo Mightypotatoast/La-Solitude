1bdc39a19624f6e47eaebb200b8d614117d6c0af
MightyPotatoast <chauvet.contact@gmail.com>
2022-07-01 12:15:32 +0200

    :/

--- src/Events/Interaction/interactionCreate.js
+++ src/Events/Interaction/interactionCreate.js
@@ -1,59 +1,54 @@
-const { MessageEmbed, CommandInteraction, Client } = require('discord.js')
-const { musicEmbed, errorEmbed } = require("../../util/Embeds") //!provisoir, a retirer quand Handler pour Select menu sera présent
+const { MessageEmbed, CommandInteraction, Client } = require("discord.js");
+const { musicEmbed, errorEmbed } = require("../../util/Embeds"); //!provisoir, a retirer quand Handler pour Select menu sera présent
 
 module.exports = {
-
     name: "interactionCreate",
-    
+
     /**
-     * 
-     * @param {CommandInteraction} interaction 
-     * @param {Client} client 
-     * @returns 
+     *
+     * @param {CommandInteraction} interaction
+     * @param {Client} client
+     * @returns
      */
     async execute(interaction, client) {
-        
         if (interaction.isCommand() || interaction.isContextMenu()) {
             const command = client.commands.get(interaction.commandName);
-            if (!command) return interaction.reply({
-                embeds: [
-                    errorEmbed()
-                        .setDescription("Une erreur est survenue lors de la récupération de la commande")
-                ], ephemeral : true
-            } ) && client.commands.delete(interaction.commandName);
-
-            command.execute(interaction,client)
-        }
-        
-        else if (interaction.isSelectMenu()) {    //! a modifer/mettre en place un handler pour les SelectMenu
+            if (!command) return;
             try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({
+                    content: "There was an error while executing this command!",
+                    ephemeral: true,
+                });
+            }
+        } else if (interaction.isSelectMenu()) {
+            //TODO a modifer/mettre en place un handler pour les SelectMenu
+            try {
+                if (interaction.customId !== "remove") return;
+                await interaction.deferReply();
+                const queue = client.distube.getQueue(interaction);
+                const songId = interaction.values[0];
+                queue.songs.splice(songId, 1);
 
-                if (interaction.customId !== 'remove') return
-                await interaction.deferReply()
-                const queue = client.distube.getQueue(interaction)
-                const songId = interaction.values[0]
-                queue.songs.splice(songId, 1)
-                
                 await interaction.followUp({
-                embeds: [
-                        musicEmbed()
-                            .setDescription(`${interaction.user} a supprimé la musique [${queue.songs[songId].name}](${queue.songs[songId].url}) de la file d'attente`)
-                    ]
-                })
-                
-                interaction.message.delete()
+                    embeds: [
+                        musicEmbed().setDescription(
+                            `${interaction.user} a supprimé la musique [${queue.songs[songId].name}](${queue.songs[songId].url}) de la file d'attente`
+                        ),
+                    ],
+                });
+
+                interaction.message.delete();
                 //interaction.message.resolveComponent(interaction.customId).setDisabled(true) //!wtf pk ca marche pas
             } catch (e) {
-                console.error(e)
-                interaction.editReply(
-                    {
-                        embeds: [
-                            errorEmbed().setDescription(`ALED : \n${e}`)
-                        ],
-                        ephemeral: true
-                    }
-                )
+                console.error(e);
+                interaction.editReply({
+                    embeds: [errorEmbed().setDescription(`ALED : \n${e}`)],
+                    ephemeral: true,
+                });
             }
         }
-    }
-}
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
-const {Client, Collection, MessageEmbed} = require("discord.js");
+require("dotenv").config();
+const { Client, Collection, MessageEmbed } = require("discord.js");
 const client = new Client({ intents: 32767 });
-const Distube = require('distube')
-
-
-client.commands = new Collection()
+const Distube = require("distube");
+const fs = require("fs");
+client.commands = new Collection();
 
 require("./Structures/Events")(client);
 require("./Structures/Commands")(client);
 
-client.distube = new Distube.default(client, { searchSongs: 0, emitNewSongOnly: true })
+client.distube = new Distube.default(client, {
+    searchSongs: 0,
+    emitNewSongOnly: true,
+});
 
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
