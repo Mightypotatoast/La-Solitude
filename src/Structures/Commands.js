7ec00956699cde19a8ad654ee6316c4b8310696f
MightyPotatoast <chauvet.contact@gmail.com>
2022-07-01 12:15:18 +0200

    client.commands.set is not a function me pete les couilles

--- src/Structures/Commands.js
+++ src/Structures/Commands.js
@@ -5,33 +5,39 @@ const { Perms } = require("../util/Permissions"),
     PG = promisify(glob),
     Ascii = require("ascii-table");
 const { REST } = require("@discordjs/rest");
+const { log } = require("console");
 const { Routes } = require("discord-api-types/v9");
 require("dotenv").config();
-const fs = require("node:fs");
+const fs = require("fs");
+const path = require("node:path");
+
 /**
  *
  * @param {Client} client
  */
 module.exports = async (client) => {
     /*******************************************/
-    //           COMMANDS UPDATE              //
+    //           COMMANDS UPDATE               //
     /*******************************************/
-
-    const commands = [];
+    client.commands = [];
     (await PG(`${process.cwd()}/src/Commands/music/*.js`)).map(async (file) => {
         const command = require(file);
-        console.log(`commande ${command.data.name} chargée 🟢`);
-        commands.push(command.data);
+        //console.log(`commande ${command.data.name} chargée 🟢`);
+        client.commands.push(command.data.toJSON());
     });
+
     const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
     (async () => {
         try {
             console.log("Started refreshing application (/) commands    .");
 
             await rest.put(
-                Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+                Routes.applicationGuildCommands(
+                    process.env.DISCORD_CLIENT_ID,
+                    "493383989588000769"
+                ),
                 {
-                    body: commands,
+                    body: client.commands,
                 }
             );
 
@@ -42,55 +48,72 @@ module.exports = async (client) => {
     })();
 
     /*******************************************/
+    //       Add commands in collection          //
+    /*******************************************/
+    const commandsPath = path.join(__dirname, "../Commands/music");
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+        console.log(`commande ${file} trouvé`);
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        console.info(command);
+        // Set a new item in the Collection
+        // With the key as the command name and the value as the exported module
+        client.commands.set(command.data.name, command);
+    }
+
+    /*******************************************/
     //           CONSOLE RECAP                 //
     /*******************************************/
-    commandsArrayScync = [];
-    const Table = new Ascii("Command Loaded");
-    CommandsArray = [];
-    (await PG(`${process.cwd()}/src/Commands/music/*.js`)).map(async (file) => {
-        const command = require(file);
+    // const Table = new Ascii("Command Loaded");
+    // CommandsArray = [];
+    // (await PG(`${process.cwd()}/src/Commands/music/*.js`)).map(async (file) => {
+    //     const command = require(file);
 
-        if (!command.name)
-            return Table.addRow(
-                file.split("/")[7],
-                "🔴 FAILED",
-                "Missing a name."
-            );
+    //     if (!command.name)
+    //         return Table.addRow(
+    //             file.split("/")[7],
+    //             "🔴 FAILED",
+    //             "Missing a name."
+    //         );
 
-        if (command.type !== "USER" && !command.description)
-            return Table.addRow(
-                command.name,
-                "🔴 FAILED",
-                "Missing a description."
-            );
+    //     if (command.type !== "USER" && !command.description)
+    //         return Table.addRow(
+    //             command.name,
+    //             "🔴 FAILED",
+    //             "Missing a description."
+    //         );
 
-        if (!command.permission) {
-            if (Perms.includes(command.permission))
-                command.defaultPermission = false;
-            else
-                return Table.addRow(
-                    command.name,
-                    "🔴 FAILED",
-                    "Permission is invalid."
-                );
-        }
+    //     if (!command.permission) {
+    //         if (Perms.includes(command.permission))
+    //             command.defaultPermission = false;
+    //         else
+    //             return Table.addRow(
+    //                 command.name,
+    //                 "🔴 FAILED",
+    //                 "Permission is invalid."
+    //             );
+    //     }
 
-        if (!command.active)
-            return Table.addRow(command.name, "⚠️  DESACTIVATED");
+    //     if (!command.active)
+    //         return Table.addRow(command.name, "⚠️  DESACTIVATED");
 
-        command.category = file.split("/")[file.split("/").length - 2];
+    //     command.category = file.split("/")[file.split("/").length - 2];
 
-        client.commands.set(command.name, command);
-        CommandsArray.push(command);
-        if (
-            typeof command.description === "string" ||
-            command.description instanceof String
-        ) {
-        } else {
-            command.description = "No description";
-        }
+    //     client.commands.set(command.name, command);
+    //     CommandsArray.push(command);
+    //     if (
+    //         typeof command.description === "string" ||
+    //         command.description instanceof String
+    //     ) {
+    //     } else {
+    //         command.description = "No description";
+    //     }
 
-        await Table.addRow(command.name, "🟢 SUCCESSFUL");
-    });
-    console.log(Table.toString());
+    //     await Table.addRow(command.name, "🟢 SUCCESSFUL");
+    // });
+    // console.log(Table.toString());
 };
