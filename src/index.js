3836c711cffb72820d5abc1338005366e3a4e932
Ayrox <eweng@live.fr>
2022-08-17 14:58:13 +0200

    add intents for distube

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-//const Distube = require("distube");
+const Distube = require("distube");
 const fs = require("fs");
 const path = require("node:path");
 
@@ -8,33 +8,28 @@ const { loadCommands } = require("./Handlers/Commands")
 
 const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder } = require("discord.js");
 
-const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits
+const {Guilds, GuildMembers, GuildMessages, GuildVoiceStates} = GatewayIntentBits
 const {User, Message, GuildMember, ThreadMember} = Partials
 
 const client = new Client({ 
-    intents: [Guilds, GuildMembers, GuildMessages],
+    intents: [Guilds, GuildMembers, GuildMessages, GuildVoiceStates],
     partials: [User, Message, GuildMember, ThreadMember] 
 });
 
 client.commands = new Collection()
 
+loadEvents(client);
 
 client
     .login(process.env.DISCORD_TOKEN)
     .then(()=> {
-        loadEvents(client);
         loadCommands(client);
     })
     .catch((err) => { console.log(err); });
         
     
     
-    
-    /*client.distube = new Distube.default(client, {
-            searchSongs: 0,
-            emitNewSongOnly: true,
-        });*/
-        
-        /*******************************************/
-        //       Add commands in collection          //
-        /*******************************************/
+client.distube = new Distube.default(client, {
+    searchSongs: 0,
+    emitNewSongOnly: true,
+});
