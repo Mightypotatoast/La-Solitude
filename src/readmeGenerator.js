1b8fb6918812ec2667d9c62290b36be27e3aff76
Ayrox <eweng@live.fr>
2022-08-18 13:23:30 +0200

    change description

--- src/readmeGenerator.js
+++ src/readmeGenerator.js
@@ -4,7 +4,8 @@ const commandsFolder = fs.readdirSync("./Commands");
 
 fs.appendFile(
     "../README.md",
-    `# La Solitude Discord Bot\n\nDepuis la disparition de Rythm et de Groovy,\nj'ai décidé de créer mon propre bot discord afin de répondre au besoin du serveur mon serveur.\n\n## Author\n\n-   [@Ayrox](https://github.com/Ayrox)\n\n## Contributors\n\n-   [@Syns369](https://github.com/Syns369)\n-   [@MightyPotatoast](https://github.com/MightyPotatoast)\n\n## Commands\n`,
+    `# La Solitude Discord Bot\n\n## Author\n\n-   [@Ayrox](https://github.com/Ayrox)\n\n## Contributors\n\n-   [@Syns369](https://github.com/Syns369)\n-   [@MightyPotatoast](https://github.com/MightyPotatoast)\n\n## Commands\n`,
+    
     function (err) {
         if (err) {
             console.log(`${folder} failed to be added to the README.md`);
